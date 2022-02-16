14acde72cf3ef4f0101660640802c05ddddd5361
smurzaliev <samat.murzaliev@gmail.com>
2022-02-16 20:34:11 +0600

    Initial Commit

---
+++ ContactApp-Murzaliev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp-Murzaliev
+//
+//  Created by Samat Murzaliev on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
